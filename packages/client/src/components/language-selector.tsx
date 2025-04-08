59413670de61a562ca03ca9b993b6b907b2e77f9
mahutt <tommy.mahut@gmail.com>
2025-04-07 23:30:44 -0400

    Remove search bar from language selector

--- packages/client/src/components/language-selector.tsx
+++ packages/client/src/components/language-selector.tsx
@@ -1,21 +1,12 @@
 import * as React from 'react'
-import { Check, Languages } from 'lucide-react'
-
-import { cn } from '@/lib/utils'
+import { Languages } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from '@/components/ui/command'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 const languages = [
   {
@@ -29,53 +20,31 @@ const languages = [
 ]
 
 export function LanguageSelector() {
-  const [open, setOpen] = React.useState(false)
   const [value, setValue] = React.useState('english')
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full justify-between"
-        >
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="w-full justify-between">
           {value
-            ? languages.find((framework) => framework.value === value)?.label
+            ? languages.find((language) => language.value === value)?.label
             : 'Select Language...'}
           <Languages className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-full p-0">
-        <Command>
-          <CommandInput placeholder="Search language..." />
-          <CommandList>
-            <CommandEmpty>No framework found.</CommandEmpty>
-            <CommandGroup>
-              {languages.map((language) => (
-                <CommandItem
-                  key={language.value}
-                  value={language.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? '' : currentValue)
-                    setOpen(false)
-                  }}
-                  disabled={language.value === value}
-                >
-                  <Check
-                    className={cn(
-                      'mr-2 h-4 w-4',
-                      value === language.value ? 'opacity-100' : 'opacity-0'
-                    )}
-                  />
-                  {language.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-68">
+        {languages.map((language) => (
+          <DropdownMenuItem
+            key={language.value}
+            onClick={() => setValue(language.value)}
+            className={`w-full ${
+              value === language.value ? 'bg-accent text-muted-foreground' : ''
+            }`}
+          >
+            {language.label}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
